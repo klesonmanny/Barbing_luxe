05b055a699161ab0234b28ace91838374918b5f9
ayomide adepoju <ayomideadepoju778@gmail.com>
2025-01-24 11:19:06 +0100

    Initial commit: Add base project files

---
+++ index.css/index.js
@@ -0,0 +1,8 @@
+const sidebar = document.querySelector('.side-bar');
+ 
+ function showSideBar(){
+    sidebar.style.display = 'flex';
+ }
+ function closeSideBar(){
+   sidebar.style.display = 'none';
+ }
